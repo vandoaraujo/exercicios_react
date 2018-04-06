15f9e3976d2dc65cc9c5897bb438066459ea1818
Vando <09356904766>
2018-04-06 11:04:13 -0300

    Criação de classes no REACT

--- ex/classComponent.jsx
+++ ex/classComponent.jsx
@@ -10,18 +10,26 @@ constructor(props){
     this.state = { value: props.initialValue } // this.state é uma variavel padrão que vem do Component.
     //atribuimos o valor de props.initialValue que vem READONLY(Todo valor é passado como READONLY para
     //os componentes. A variavel value recebe esse valor e atualizamos o this.state)
+
 }
 
 sum(delta){
     this.setState( { value: this.state.value + delta } )
+    //Fazemos assim porque o estado deve ser evoluído e não modificado diretamente.
+    //Fazendo desse jeito criamos uma especie de historico da evolução do valor...
 } 
-
+/**
+ * usada uma funcao arrow, pois
+   se fizermos direto this.sum(1), o retorno daria undefined e ligariamos esse
+   undefined ao evento onclick, então queremos passar uma função para o onclick
+   e não o retorno dessa funcao. Então passamos a funcao arrow e dentro dela passamos a função sum
+ */
 render(){
     return(
         <div>
            <h1>{this.props.label}</h1>
            <h2>{this.state.value}</h2>
-           <button onClick={() => this.sum(-1)}> Dec </button>
+           <button onClick={() => this.sum(-1)}> Dec </button> 
            <button onClick={() => this.sum(1)}> Inc </button>
         </div>    
     )
