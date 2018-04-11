5492c7840fe6462155013a81ae3226d2db7b4f27
Vando <09356904766>
2018-04-11 14:37:20 -0300

    frontend react

--- todo-app/backend/src/config/cors.js
+++ todo-app/backend/src/config/cors.js
@@ -3,8 +3,13 @@ module.exports = function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH,DELETE')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept')
     next()
+    //se não tivesse o next esse midleware iria travar a aplicação
  }
  /** Isso é um midleware bastante comum usando
   o padrão do express. O next é conhecido como CHAIN
   do chain of responsability, se ele vai pro proximo
-  midleware ou se vai abortar... */
+  midleware ou se vai abortar...
+  Adicionamos um cabeçalho na resposta que será enviada
+ para dizer que é permitido
+  acessar o serviço... 
+  Com isso a nossa API está aberta para ser consumida... */
