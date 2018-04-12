153ee2175dcb6ac3d67dc4dbbe6ae07b34b85fa0
Vando <09356904766>
2018-04-12 09:56:17 -0300

    front react

---
+++ todo-app/frontend/src/main/app.jsx
@@ -0,0 +1,17 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+/**
+ * Esses imports são do sistema de modulos do ECMA Script 2015,
+ * que são o import e o export que é um sistema similar ao que tem
+ * no NODE, mas o node USa o require que é o padrão do COMMON.JS
+ * e esse padrão é do ECMA SCRIPT 2015
+ */
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)
