88296efc07bd488511e626de93d39113c15fdd83
Vando <09356904766>
2018-04-12 13:51:04 -0300

    adicao menu

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -6,12 +6,19 @@ import 'modules/font-awesome/css/font-awesome.min.css'
  * que são o import e o export que é um sistema similar ao que tem
  * no NODE, mas o node USa o require que é o padrão do COMMON.JS
  * e esse padrão é do ECMA SCRIPT 2015
+ * O Webpack roda nos dois...poderíamos fazer acima um require
  */
 
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about';
+import Menu from '../template/menu'
+
 
 export default props => (
     <div className='container'>
-        <h1>Teste</h1>
+       <Menu />
+       <Todo />
+       <About />
     </div>
 )
