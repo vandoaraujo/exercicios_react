3e227a1a50d5901f50569448950a2e2e32d4b392
Vando <09356904766>
2018-07-02 15:03:29 -0300

    servicos

--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -7,9 +7,13 @@ export default props => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='danger' icon='trash-o'
+                    <IconButton style='sucess' icon='check' hide={todo.done}
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                    <IconButton style='warning' icon='undo' hide={!todo.done}
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
                         onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
             </tr>
