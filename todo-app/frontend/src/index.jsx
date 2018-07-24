02dae70e5c49150a41267512446b1a0d0d00fc0c
Vando Araujo <tecladista.vando@gmail.com>
2018-07-23 22:14:33 -0300

    redux
    acertos commit redux

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/app'
+import {combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Field from './field'
+
+const reducers = combineReducers({
+    field: () => ({value: 'opa'})
+})
 
 ReactDOM.render(<App/>, document.getElementById('app'))
