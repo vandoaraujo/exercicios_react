0a240e807d7396430f9a8655edae76b4b6157684
Vando <09356904766>
2018-09-10 16:40:38 -0300

    configuracao reducers react

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Field from './ex/field'
+import fieldReducer from './ex/fieldReducer'
 
 const reducers = combineReducers({
-    field: ()=> ({ value: 'Opa' })
+    //field: ()=> ({ value: 'Opa' })
+    field: fieldReducer
 })
 
 ReactDOM.render(
         <Provider store={createStore(reducers)}>
              <Field initialValue='Teste' />  
         </Provider>
-        , document.getElementById('app'))
+        , document.getElementById('app')
+)
