9ae3853737c497899795533a1a5de3fe6bd33cc2
vandoaraujo <tecladista.vando@gmail.com>
2018-12-18 14:18:10 -0200

    commit

--- todo-app/my-money-app/backend/src/config/cors.js
+++ todo-app/my-money-app/backend/src/config/cors.js
@@ -1,6 +1,14 @@
 module.exports = (req, res, next) => {
+
+    /**
+     * Compartilhamento de recursos de origens cruzadas (CORS) é um mecanismo que usa
+     * cabeçalhos HTTP adicionais para garantir permissão de acesso a recursos de um
+     * servidor diferente do da origem da página (domínio) correntemente em uso.
+     */
+    
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    //O next é necessário para continuar o fluxo, pois o middleware não trata a resposta.
     next()
 }
