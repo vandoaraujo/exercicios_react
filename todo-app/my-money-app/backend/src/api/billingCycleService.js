d2154d1b818f560b8b1e6a7ead51d137144390cf
vandoaraujo <tecladista.vando@gmail.com>
2018-12-18 12:28:51 -0200

    commit

--- todo-app/my-money-app/backend/src/api/billingCycleService.js
+++ todo-app/my-money-app/backend/src/api/billingCycleService.js
@@ -1,4 +1,5 @@
 const BillingCycle = require('./billingCycle.js')
+//FAÇO O IMPORT DO MIDDLEWARE DE TRATAMENTO DE ERRO
 const errorHandler = require('./common/errorHandler.js')
 
 //Criação dos serviços Web através do Express
@@ -6,6 +7,9 @@ BillingCycle.methods(['get','post','put','delete'])
 BillingCycle.updateOptions({new:true , runValidators: true}) //no NODERESTFUL, o update não obriga o preenchimento
 //dos campos, então precisamos informar essa opção para obrigar a validação também no update. O new informa que o
 // objeto atualizado em um put será retornado
+
+//Esse método permite interceptar a requisição depois do POST/PUT para aplicar o middleware de erro
+// efetuando o tratamento correto·
 BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('count', (req, res, next) => {
@@ -29,17 +33,30 @@ BillingCycle.route('count', (req, res, next) => {
  */
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate({
-        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
+        $project: {
+            credit: {$sum: "$credits.value"},
+            debt: {$sum: "$debts.value"}
+        }
     }, {
-        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+        //agrupa os valores por base num criterio. A API do mongo exige que exista um id=null
+        //A variavel $credit é o resultado da variavel credit de project acima,
+        $group:{
+            _id: null, // esse nul informa que a query retornará o resultado geral de tudo, se eu colocasse
+            //ano por exemplo retornaria os valores agrupados por ano.
+            credit: {$sum: "$credit"},
+            debt: {$sum: "$debt"}
+        }
        },
        {
+        //Essa linha indica que do resultado da pipeline, queremos exibir apenas o credit e o debt.
+        //O valor 0 indica que o id, não será exibido.
         $project: {_id: 0, credit: 1, debt: 1}
-       },
+       },//Depois que a pipeline for executada, essa calback será chamada.
     (error, result) => {
         if(error) {
             res.status(500).json({errors: [error]})
         }else {
+            // A resposta é um array, então eu passo o indice zero do result.
             res.json(result[0] || { credit: 0, debt: 0 })
         }
     })
--- todo-app/my-money-app/backend/src/api/common/errorHandler.js
+++ todo-app/my-money-app/backend/src/api/common/errorHandler.js
@@ -1,16 +1,20 @@
 const _ = require('lodash')
+
+//Crio um middleware exportando-o e passando os parametros necessários de req, resp e next.
 module.exports = (req, res, next) => {
     const bundle = res.locals.bundle
     if(bundle.errors) {
         const errors = parseErrors(bundle.errors)
+        //essa sintaxe é do ecmascript 2015
         res.status(500).json({errors})
     } else {
+        //se nao tiver nenhum erro chamo um next, já que isso é um middleware.
         next()
     }
 }
     
 const parseErrors = (nodeRestfulErrors) => {
-const errors = []
-    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+const errors = [];
+_.forIn(nodeRestfulErrors, error => errors.push(error.message))
         return errors
 }
