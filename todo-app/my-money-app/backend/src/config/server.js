53b4e9f35248d1f4cbe9f28fded22af228e6ab08
vandoaraujo <tecladista.vando@gmail.com>
2018-12-18 14:56:55 -0200

    commit

--- todo-app/my-money-app/backend/src/config/server.js
+++ todo-app/my-money-app/backend/src/config/server.js
@@ -4,10 +4,11 @@ const bodyparser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+//É um parser que é aplicado no fluxo de requisição e faz o cast de String para numérico das queryparams.
 const queryParser = require('express-query-int')
 
 server.use(bodyparser.urlencoded({ extended: true}))
-server.use(bodyparser.json())
+server.use(bodyparser.json())   
 server.use(allowCors)
 server.use(queryParser())
 
