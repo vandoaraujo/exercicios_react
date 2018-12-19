761b9ec3ed23f83366150dfc80ccda16be36a194
vandoaraujo <tecladista.vando@gmail.com>
2018-12-19 09:58:54 -0200

    app frontend em react e redux -  integração com backend node-rest, mongo, mongoose

---
+++ todo-app/my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+<footer className='main-footer'>
+    <strong>
+        Copyright &copy; 2018
+        <a href='http://serpro.gov.br' target='_blank'> Serpro</a>.
+    </strong>
+</footer>
+)
