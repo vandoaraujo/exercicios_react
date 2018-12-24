865d9b20c28bed8935a8ae24495eff6f741fabf1
vandoaraujo <tecladista.vando@gmail.com>
2018-12-24 10:06:37 -0200

    correcao

--- todo-app/my-money-app/frontend/src/index.jsx
+++ todo-app/my-money-app/frontend/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import promise from 'redux-promise'
 
