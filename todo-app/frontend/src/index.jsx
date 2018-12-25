e9def6b26a438a106074ada8ff595d6388da8b67
Vando Araujo <tecladista.vando@gmail.com>
2018-12-25 20:28:22 -0200

    Please enter the commit message for your changes. Lines starting merge
    :wq

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware} from 'redux'
 import { Provider } from 'react-redux'
 
-import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
