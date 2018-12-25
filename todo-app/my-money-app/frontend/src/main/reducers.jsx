8ee34ee57e153b5852503e797a2262eb70a1918b
Vando Araujo <tecladista.vando@gmail.com>
2018-12-25 21:21:00 -0200

    merge

--- todo-app/my-money-app/frontend/src/main/reducers.jsx
+++ todo-app/my-money-app/frontend/src/main/reducers.jsx
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import DashBoardReducer from '../dashboard/dashBoardReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
-    dashboard: DashBoardReducer
+    dashboard: DashBoardReducer,
+    tab: TabReducer
 })
 export default rootReducer
 
