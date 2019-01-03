4b72a6f6e769017bc6f54bc09b4c9c4e7ac9c4d9
vandoaraujo <tecladista.vando@gmail.com>
2019-01-03 14:37:59 -0200

    adição features

--- todo-app/my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ todo-app/my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,11 +6,23 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
+
+    calculateSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
+
     render() {
-        const { handleSubmit, readOnly, credits }  = this.props
+        const { handleSubmit, readOnly, credits, debts }  = this.props
+        const { sumOfCredits, sumOfDebts } = this.calculateSummary()
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
@@ -20,7 +32,11 @@ class BillingCycleForm extends Component {
                         label='Mês' cols='12 4' placeholder='Informe o mês' />
                     <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
-                        <CreditList cols='12 6' list={credits} readOnly={readOnly} />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
+                    <ItemList cols='12 6' list={credits} field='credits' readOnly={readOnly} 
+                            legend='Créditos'/>
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts'
+                            legend='Débitos' showStatus={true} />
                 </div>
                 <div className='box-footer'>
                 <button type='submit' className={`btn btn-${this.props.submitClass}`}>
@@ -44,7 +60,7 @@ BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount:false})
 const selector  = formValueSelector('billingCycleForm')
 //a funcao selector recebe o state como propriedade e a string credits e retorna o array de creditos
 //que será colocado dentro do state.
-const mapStateToProps = state => ({credits: selector(state, 'credits')})
+const mapStateToProps = state => ({credits: selector(state, 'credits'), debts: selector(state, 'debts')})
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
 //o mapstatetoprops irá nulo, pois não temos ele e não precisamos
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
