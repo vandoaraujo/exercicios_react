acc084b9bf161c786cdc5816fb1ba7c89863d749
vandoaraujo <tecladista.vando@gmail.com>
2019-01-07 10:59:46 -0200

    adição features auth

--- todo-app/my-money-app/backend/src/api/user/authService.js
+++ todo-app/my-money-app/backend/src/api/user/authService.js
@@ -5,6 +5,7 @@ const User = require('./user')
 const env = require('../../.env')
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
+
 const sendErrorsFromDB = (res, dbErrors) => {
   const errors = []
   _.forIn(dbErrors.errors, error => errors.push(error.message))
@@ -28,22 +29,22 @@ const login = (req, res, next) => {
             return res.status(400).send({errors: ['Usuário/Senha inválidos']})
         }
     })
-  }
+}
 
-  const validateToken = (req, res, next) => {
+const validateToken = (req, res, next) => {
     const token = req.body.token || ''
     jwt.verify(token, env.authSecret, function(err, decoded) {
-    return res.status(200).send({valid: !err})
+        return res.status(200).send({valid: !err})
     })
-  }
+}
   
-  const signup = (req, res, next) => {
+const signup = (req, res, next) => {
     const name = req.body.name || ''
     const email = req.body.email || ''
     const password = req.body.password || ''
     const confirmPassword = req.body.confirm_password || ''
     if(!email.match(emailRegex)) {
-        return res.status(400).send({errors: ['O e-mail informa está inválido']})
+        return res.status(400).send({errors: ['O e-mail informado está inválido']})
     }
     if(!password.match(passwordRegex)) {
         return res.status(400).send({errors: [
@@ -71,6 +72,6 @@ const login = (req, res, next) => {
         })
     }
     })
-  }
+}
 
-  module.exports = { login, signup, validateToken }
+module.exports = { login, signup, validateToken }
