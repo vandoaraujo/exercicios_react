bc438dbe7cf81a11acd3a2176906a6e4ea194bae
vandoaraujo <tecladista.vando@gmail.com>
2019-03-22 11:49:26 -0300

    exerc nodes

---
+++ FundamentosMEAN/node/ex08_process.js
@@ -0,0 +1,10 @@
+function temParam(param) {
+   return process.argv.indexOf(param) !== -1
+}
+
+if(temParam('--producao')){
+    console.log("Atencao Total")
+}else{
+    console.log("tranquilo...")
+}
+
