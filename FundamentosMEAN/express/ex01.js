2c7829bc308cb4cb9858ed62d351a417b83264c0
Vando Araujo <tecladista.vando@gmail.com>
2019-03-23 16:00:42 -0300

    primeiro exemplo express

---
+++ FundamentosMEAN/express/ex01.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const server = express()
+
+//formas de mapear uma URL com o express
+//Mapeando o GET - retorna o HTML para a pagina
+server.get('/', function(req, res){
+    res.send('<h1>Index!</h1>')
+})
+
+//mapeando GET e post na URL /teste
+server.all('/teste', function(req, res){
+    res.send('<h1>Teste! </h1>')
+})
+
+server.get('/api/', function(req, res){
+    res.send('<h1>API!</h1>')
+})
+
+//Escutando na porta 3000
+server.listen(3000, () => console.log('Executando...'))
