5583fa969b3391d8d1b00ac7b16e8890eea9ea86
vandoaraujo <tecladista.vando@gmail.com>
2019-03-25 10:39:31 -0300

    novos exerc express

--- FundamentosMEAN/express/ex05.js
+++ FundamentosMEAN/express/ex05.js
@@ -2,22 +2,8 @@ const express = require('express')
 const server = express()
 const router = require('./ex05_router')
 
-server.use(router)
-
-// //formas de mapear uma URL com o express
-// //Mapeando o GET - retorna o HTML para a pagina
-// server.get('/', function(req, res){
-//     res.send('<h1>Index!</h1>')
-// })
-
-// //mapeando GET e post na URL /teste
-// server.all('/teste', function(req, res){
-//     res.send('<h1>Teste! </h1>')
-// })
-
-// server.get('/api/', function(req, res){
-//     res.send('<h1>API!</h1>')
-// })
+//o router será uma função middleware.
+server.use('/api', router)
 
 //Escutando na porta 3000
 server.listen(3000, () => console.log('Executando...'))
