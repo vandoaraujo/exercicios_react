3a5a519c1572a1a7c639d4cb4987682bdace57f5
Vando Araujo <tecladista.vando@gmail.com>
2019-03-25 22:28:32 -0300

    acerto pacotes npm bcrypt e visualizacao porta

--- todo-app/my-money-app/backend/src/config/server.js
+++ todo-app/my-money-app/backend/src/config/server.js
@@ -13,7 +13,7 @@ server.use(allowCors)
 server.use(queryParser())
 
 server.listen(port , function(){
-    console.log('BackEnd is running on port ${port}.');
+    console.log(`BackEnd is running on port ${port}.`);
 })
 
 module.exports = server
