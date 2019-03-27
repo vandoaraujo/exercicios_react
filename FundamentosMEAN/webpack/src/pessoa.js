cbcf023ab93bee7a2fd54a79cfb128edd92b6a86
vandoaraujo <tecladista.vando@gmail.com>
2019-03-27 13:55:59 -0300

    adicao exercicio webpack

---
+++ FundamentosMEAN/webpack/src/pessoa.js
@@ -0,0 +1,11 @@
+export default class Pessoa{
+    cumprimentar(){
+        return 'Bom dia!'
+    }
+}
+
+/** module.exports = class Pessoa{
+    cumprimentar(){
+        return 'Bom dia!'
+    }
+} */
---
+++ FundamentosMEAN/webpack/src/principal.js
@@ -0,0 +1,9 @@
+import Pessoa from './pessoa'
+/**
+ * Usamos desse modo pois estamos usando o padrao
+ * do ECMA Script
+ * 
+ */
+//const Pessoa = require('./pessoa')
+const atendente = new Pessoa
+console.log(atendente.cumprimentar())
